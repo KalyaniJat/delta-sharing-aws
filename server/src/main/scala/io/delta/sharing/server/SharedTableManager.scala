fada11e27cac55dae0bcfd98e607de000628b99c
Kalyani Jat <kalyani.jat@nagarro.com>
2025-05-06 11:58:14 +0530

    changes for yaml file

--- server/src/main/scala/io/delta/sharing/server/SharedTableManager.scala
+++ server/src/main/scala/io/delta/sharing/server/SharedTableManager.scala
@@ -78,7 +78,8 @@ class SharedTableManager(serverConfig: ServerConfig) {
         }
       } else {
         // scalastyle:off println
-        println(s"WARNING: Share or Schema not found in YAML for share=$shareName, schema=$schemaName")
+        println(s"WARNING: Share=$shareName")
+        println(s"WARNING: Schema=$schemaName")
         println("WARNING: Skipping tables from DB because share/schema not matched")
         // scalastyle:on println
       }
